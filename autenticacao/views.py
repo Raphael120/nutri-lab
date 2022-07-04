aa1f24c6f1da32b8cf7b320c512fc878d1deb9b1
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-07-04 18:55:55 -0300

    configurando URLs de autenticação

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,3 +1,12 @@
+from urllib import response
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def cadastro(request):
+    pass
+
+def logar(request):
+    pass
