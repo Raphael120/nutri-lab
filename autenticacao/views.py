d102684e8c2b216f5cbbb1e9c92c10fe422dbcaa
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-07-07 01:32:20 -0300

    criando a view function para cadastro de usuários

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,12 +1,56 @@
-from urllib import response
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+from .utils import email_is_valid, password_is_valid, username_is_valid
 
 # Create your views here.
 
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+
+    elif request.method == 'POST':
+        usuario = request.POST['usuario']
+        email = request.POST['email']
+        senha = request.POST['senha']
+        confirmar_senha = request.POST['confirmar_senha']
+
+        if not username_is_valid(request, usuario):
+            return redirect('/auth/cadastro')
+
+        if not email_is_valid(request, email):
+            return redirect('/auth/cadastro')
+
+        if not password_is_valid(request, senha, confirmar_senha):
+            return redirect('/auth/cadastro')
+
+        # Verifica se o nome de usuário já está cadastrado:
+        user = User.objects.filter(username=usuario)
+
+        if user.exists():
+            messages.add_message(request, constants.ERROR,
+                                 f'Esse nome de usuário já está cadastrado')
+            return redirect('/auth/cadastro')
+
+        try:
+            user = User.objects.create_user(username=usuario,
+                                            email=email,
+                                            password=senha,
+                                            is_active=False)
+            user.save()
+            messages.add_message(request, constants.SUCCESS,
+                                 'Usuário criado com sucesso.')
+            return redirect('/auth/logar')
+
+        except:
+            messages.add_message(request, constants.ERROR,
+                                 'Erro interno do sistema.')
+            return redirect('/auth/cadastro')
+
 
 def logar(request):
-    pass
+    return HttpResponse('Página de login')
