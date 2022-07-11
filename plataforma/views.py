abb5002bf567cc20b348439bb51f9fe00068eda0
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-07-11 20:36:29 -0300

    adicionando a view para a página de pacientes

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+@login_required(login_url='/auth/logar')
+def pacientes(request):
+    return render(request, 'pacientes.html')
