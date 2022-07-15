87fcca7c80dfa6ac67b40419825b14c407d2d7cc
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-07-14 23:21:01 -0300

    adicionando urls para as páginas de dados do paciente

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('pacientes/', views.pacientes, name='pacientes')
+    path('pacientes/', views.pacientes, name='pacientes'),
+    path('dados_paciente/', views.listar_dados_paciente, name='listar_dados_paciente'),
+    path('dados_paciente/<str:id>', views.dados_paciente, name='dados_paciente')
 ]
