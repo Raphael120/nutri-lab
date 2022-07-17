b0f8b594e0e54452e7dd475f88e1e813dac8bb49
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-07-17 18:27:20 -0300

    adicionando views para listagem e cadastro de dados dos pacientes

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,11 +1,14 @@
+from datetime import datetime
+
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages import constants
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from .utils import paciente_is_valid
+from django.shortcuts import get_object_or_404, redirect, render
 
-from plataforma.models import Paciente
+from plataforma.models import DadosPaciente, Paciente
+
+from .utils import dados_paciente_is_valid, paciente_is_valid
 
 # Create your views here.
 
@@ -53,3 +56,79 @@ def pacientes(request):
                 message='Erro interno do sistema'
             )
             return redirect('/pacientes/')
+
+
+@login_required(login_url='/auth/logar')
+def listar_dados_paciente(request):
+    if request.method == 'GET':
+        pacientes = Paciente.objects.filter(nutricionista=request.user)
+        return render(request, 'listar_dados_paciente.html', {'pacientes': pacientes})
+
+
+@login_required(login_url='/auth/logar')
+def dados_paciente(request, id):
+    paciente = get_object_or_404(Paciente, id=id)
+
+    if not paciente.nutricionista == request.user:
+        messages.add_message(
+            request=request,
+            level=constants.ERROR,
+            message='Esse paciente não é seu'
+        )
+        return redirect('/dados_paciente/')
+
+    if request.method == 'GET':
+        dados_do_paciente = DadosPaciente.objects.filter(paciente=paciente)
+
+        context = {
+            'paciente': paciente,
+            'dados_do_paciente': dados_do_paciente
+        }
+
+        return render(request, 'dados_paciente.html', context)
+
+    elif request.method == 'POST':
+        # Dados do paciente:
+        peso = request.POST['peso']
+        altura = request.POST['altura']
+        gordura = request.POST['gordura']
+        musculo = request.POST['musculo']
+
+        # Dados laboratoriais:
+        hdl = request.POST['hdl']
+        ldl = request.POST['ldl']
+        colesterol_total = request.POST['ctotal']
+        triglicerideos = request.POST['triglicerídeos']
+
+        if not dados_paciente_is_valid(request, peso, altura, gordura, musculo, hdl, ldl, colesterol_total, triglicerideos):
+            return redirect(f'/dados_paciente/{paciente.pk} ')
+
+        try:
+            dp = DadosPaciente(
+                paciente=paciente,
+                data=datetime.now(),
+                peso=peso,
+                altura=altura,
+                percentual_gordura=gordura,
+                percentual_musculo=musculo,
+                colesterol_hdl=hdl,
+                colesterol_ldl=ldl,
+                colesterol_total=colesterol_total,
+                triglicerideos=triglicerideos
+            )
+            dp.save()
+
+            messages.add_message(
+                request=request,
+                level=constants.SUCCESS,
+                message='Dados cadastrados com sucesso'
+            )
+            return redirect('/dados_paciente/')
+
+        except:
+            messages.add_message(
+                request=request,
+                level=constants.ERROR,
+                message='Erro interno do sistema'
+            )
+            return redirect('/dados_paciente/')
