bce2615c16f1f8c035e64aa08fe6ec536c0b2601
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-07-23 21:24:51 -0300

    adicionando os modelos 'Refeicao' e 'Opcao'

--- plataforma/admin.py
+++ plataforma/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import DadosPaciente, Paciente
+from .models import DadosPaciente, Opcao, Paciente, Refeicao
 
 # Register your models here.
 
 admin.site.register(Paciente)
 admin.site.register(DadosPaciente)
+admin.site.register(Refeicao)
+admin.site.register(Opcao)
